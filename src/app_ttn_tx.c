9affbffc11b2af3066060953a4fc38af2a8dc5f6
narblaitiin <regis.rousseau@insa-lyon.fr>
2025-04-17 09:45:40 +0200

    add comments

--- src/app_ttn_tx.c
+++ src/app_ttn_tx.c
@@ -10,30 +10,42 @@
 #include "app_adc.h"
 
 //  ========== globals =====================================================================
+// define a stack for the LoRaWAN thread with a size of 1024 bytes
 K_THREAD_STACK_DEFINE(lorawan_stack, 1024);
+
+// declare a thread structure to manage the LoRaWAN thread's data
 struct k_thread lorawan_thread_data;
 
 //  ========== lorawan_thread ==============================================================
+// LoRaWAN thread function: handles ADC data acquisition and sends it over LoRaWAN
 static void lorawan_thread(void *arg1, void *arg2, void *arg3) {
+    // allocate a buffer for ADC data, sized to hold ADC_BUFFER_SIZE samples of 16-bit values
     uint8_t data[ADC_BUFFER_SIZE * sizeof(uint16_t)];
 
     while (1) {
-        k_thread_suspend(k_current_get()); // Wait to be triggered
+        // suspend the thread, effectively pausing it until explicitly resumed
+        k_thread_suspend(k_current_get());
 
+        // acquire ADC data into the buffer
         adc_get_buffer((uint16_t *)data, ADC_BUFFER_SIZE, 0);
 
+        // transmit the data over LoRaWAN
         lorawan_send(LORAWAN_PORT, data, sizeof(data), LORAWAN_MSG_UNCONFIRMED);
         printk("data sent to LoRaWAN");
     }
 }
 
-//  ========== lorawan_trigger_tx ==========================================================
+//  ========== lorawan_trigger_transmission ================================================
+// function to trigger the LoRaWAN thread to resume execution.
 void lorawan_trigger_transmission(void) {
+    // resume the suspended LoRaWAN thread, allowing it to execute.
     k_thread_resume(&lorawan_thread_data);
 }
 
 //  ========== lorawan_start ===============================================================
+// function to initialize and start the LoRaWAN transmission thread
 int lorawan_start_tx(void) {
+    // create the LoRaWAN thread with the defined stack and function
     k_thread_create(&lorawan_thread_data, lorawan_stack, K_THREAD_STACK_SIZEOF(lorawan_stack),
                     lorawan_thread, NULL, NULL, NULL, 3, 0, K_NO_WAIT);
 }
