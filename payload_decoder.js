0c55b9f08cb5df7c580bb0685e200889faa242a2
narblaitiin <regis.rousseau@insa-lyon.fr>
2025-06-18 13:19:18 +0200

    timestamp in uint64 serialization

--- payload_decoder.js
+++ payload_decoder.js
@@ -2,18 +2,18 @@ function decodeUplink(input) {
     // input payload is an array of bytes (e.g., input.bytes)
     var bytes = input.bytes;
 
-    // decode the int32 timestamp (big-endian representation)
-    var unixTimestamp = (bytes[0] << 24 >>> 0) | (bytes[1] << 16) | (bytes[2] << 8) | bytes[3]; // use `>>> 0` to ensure unsigned shift
+    // decode the uint64 timestamp (big-endian representation)
+    var unixTimestamp = (bytes[0] << 56 >>> 0) | (bytes[1] << 48) | (bytes[2] << 40) | bytes[3] << 32) | bytes[4] << 24) | bytes[5] << 16) | bytes[6] << 8) | bytes[7]; // use `>>> 0` to ensure unsigned shift
 
     // convert and display Unix timestamp as a human-readable date and time 
     var date = new Date(unixTimestamp * 1000); // convert seconds to milliseconds
     var readable_date = date.toISOString(); // ISO format (YYYY-MM-DDTHH:mm:ss.sssZ)
 
     // decode the int16 values (big-endian representation)
-    var battery = (bytes[4] << 8) | bytes[5];
-    var temperature = (bytes[6] << 8) | bytes[7];
-    var humidity = (bytes[8] << 8) | bytes[9];     
-    var velocity = (bytes[10] << 8) | bytes[11];
+    var battery = (bytes[8] << 8) | bytes[9];
+    var temperature = (bytes[10] << 8) | bytes[11];
+    var humidity = (bytes[12] << 8) | bytes[13];     
+    var velocity = (bytes[14] << 8) | bytes[15];
 
     // convert to signed 16-bit integers
     if (battery & 0x8000) battery -= 0x10000;
